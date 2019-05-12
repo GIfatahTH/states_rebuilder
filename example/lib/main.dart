b951a5263a17ffa5c57e7793f69cb97ef04d3152
MELLATI <meltft@gmail.com>
2019-05-12 14:13:43 +0100

    update example to account for enum

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 
+// enum is preferred over String to name your `stateID` for big projects.
+// The nume of the enum is of your choice. You can have many enums.
+
+// -- Conventionally for each of your BloCs you define a corresponding enum.
+// -- For very large projects you can make all your enums in a single file.
+enum CounterState { myCounter1, total }
+
 // Our logic class a counter variable and a method to increment it.
 //
 // It must extend from StatesRebuilder.
-
-enum CounterState { myCounter }
-
 class CounterBloc extends StatesRebuilder {
   int _counter1 = 0;
   int _counter2 = 0;
@@ -15,23 +19,37 @@ class CounterBloc extends StatesRebuilder {
   int get counter2 => _counter2;
 
   void increment1() {
-    // First, increment the counter
+    // Increment the counter
     _counter1++;
 
     // First alternative.
-    // Use the ids parameters to enter a list of ids.
-    // Widgets with these ids will rebuild to reflect the new counter value.
-    rebuildStates([null, 1, CounterState.myCounter, "jlll"]);
+    // Widgets with these stateIDs will rebuild to reflect the new counter value.
+    rebuildStates([CounterState.myCounter1, CounterState.total]);
   }
 
   void increment2(State state) {
-    // First, increment the counter
+    // Increment the counter
     _counter2++;
 
-    // First alternative.
-    // Use the ids parameters to enter a list of ids.
-    // Widgets with these ids will rebuild to reflect the new counter value.
-    rebuildStates([state]);
+    // Second alternative.
+    // Widgets from which the increment2 method is called will rebuild.
+    // You can mix states and stateIDs
+    rebuildStates([state, CounterState.total]);
+  }
+
+  void increment3() {
+    // increment the counter
+    _counter1++;
+
+    // The all alternative
+
+    // `rebuildStates()` with no parameter: All widgets that are wrapped with `StateBuilder` and
+    // are given `stateID` will rebuild to reflect the new counter value.
+    //
+    // you get a similar behavior like in scoped_model or provider packages
+    rebuildStates();
+    // in this particular example we have two widgets that have
+    // a stateID (CounterState.myCounter, and CounterState.total)
   }
 }
 
@@ -42,8 +60,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // At the top level of our widget tree where we want to provide the CounterBloc to all its children, we create a StateBuilder Widget.
-    // We instantiate the counterBloc variable in the initState parameter, and kill it in the dispose parameter.
+    // Provide your BloC
     return BlocProvider<CounterBloc>(
       bloc: CounterBloc(),
       child: MaterialApp(
@@ -66,49 +83,82 @@ class Counter extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('You have pushed the button this many times:'),
-            Divider(),
-            Text('The first alternative'),
+            //All widget that has a stateID will be rebuilt.
+            //look at the increment3() method in the BloC.
+            Text('The all alternative'),
+            RaisedButton(
+              child: Text("rebuildStates()"),
+              onPressed: counterBloc.increment3,
+            ),
 
-            // First Alternative:
-            // -- Wrap the Text widget with StateBuilder widget and give it and id of your choice.
-            // -- Declare the blocs where you want the state to be available.
-            StateBuilder(
-              stateID: CounterState.myCounter,
-              blocs: [counterBloc],
-              builder: (State state) => Text(
-                    counterBloc.counter1.toString(),
-                    style: Theme.of(state.context).textTheme.display1,
-                  ),
+            Divider(),
+
+            Text('The first alternative'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IncrementFromOtheWidget(),
+                // First Alternative:
+                // -- Wrap the Text widget with StateBuilder widget and give it and id of your choice.
+                // -- Declare the blocs where you want the state to be available.
+                StateBuilder(
+                  stateID: CounterState.myCounter1,
+                  blocs: [counterBloc],
+                  builder: (State state) => Text(
+                        counterBloc.counter1.toString(),
+                        style: Theme.of(state.context).textTheme.display1,
+                      ),
+                ),
+              ],
             ),
 
             // The first method is mainly useful if you want to increment the counter from other widget
-            IncrementFromOtheWidget(),
+
             Divider(),
+
             Text('The second alternative'),
 
             // Second Alternative:
             // -- Wrap the Text widget with StateBuilder widget without giving it an id.
-            // -- Declare the blocs where you want the state to be available.
+            // -- Pass the state parameter to the increment2 method.
             StateBuilder(
-              builder: (State state) => Column(
+              builder: (State state) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
+                      RaisedButton(
+                        key: Key("secondAlternative"),
+                        child: Text("increment from the same widget"),
+                        onPressed: () => counterBloc.increment2(state),
+                      ),
                       Text(
                         counterBloc.counter2.toString(),
                         style: Theme.of(state.context).textTheme.display1,
                       ),
-                      RaisedButton(
-                        key: Key("secondAlternative"),
-                        child: Text("increment the same widget"),
-                        onPressed: () => counterBloc.increment2(state),
-                      )
                     ],
                   ),
             ),
+
+            Divider(),
+
+            StateBuilder(
+              stateID: CounterState.total,
+              blocs: [counterBloc],
+              builder: (_) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("The sum of the two counters is"),
+                      Text(
+                        " ${counterBloc._counter1 + counterBloc._counter2}",
+                        style: Theme.of(context).textTheme.display1,
+                      )
+                    ],
+                  ),
+            )
           ],
         ),
       ),
