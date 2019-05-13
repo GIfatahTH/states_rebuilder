6dd9e1046de510480aa476b113c9300712586393
MELLATI <meltft@gmail.com>
2019-05-13 17:10:28 +0100

    update test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -6,7 +6,7 @@ import '../lib/main.dart';
 void main() {
   testWidgets('Counters increments', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CounterTabApp());
 
     // Verify that our both counters start at 0.
     expect(find.text('0'), findsNWidgets(2));
@@ -17,16 +17,7 @@ void main() {
     await tester.tap(find.byKey(Key("firstAlternative")));
     await tester.pump();
 
-    // Verify that only counter1 has incremented.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsOneWidget);
-
-    // Tap the button  with Key("secondAlternative") and trigger a frame.
-    // this is the second alternative
-    await tester.tap(find.byKey(Key("secondAlternative")));
-    await tester.pump();
-
-    // Verify that both counters have incremented.
+    // Verify that  counter1 and total have incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsNWidgets(2));
   });
