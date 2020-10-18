88a8495dba63a33e3dba4e7dd4b22c46616bae56
GIfatahTH <meltft@gmail.com>
2020-10-18 23:38:18 +0100

    add docs and comments

--- states_rebuilder_package/lib/src/injected/injected_persistance.dart
+++ states_rebuilder_package/lib/src/injected/injected_persistance.dart
@@ -3,6 +3,74 @@ part of '../injected.dart';
 ///PersistStore Interface to implementation.
 ///
 ///You don't have to use try-catch, as it is done by the library.
+///
+///# Examples
+///## SharedPreferences:
+///```dart
+///class SharedPreferencesImp implements IPersistStore {
+///  SharedPreferences _sharedPreferences;
+///
+///  @override
+///  Future<void> init() async {
+///    //Initialize the plugging
+///    _sharedPreferences = await SharedPreferences.getInstance();
+///  }
+///
+///  @override
+///  Object read(String key) {
+///      return _sharedPreferences.getString(key);
+///  }
+///
+///  @override
+///  Future<void> write<T>(String key, T value) async {
+///      return _sharedPreferences.setString(key, value as String);
+///  }
+///
+///  @override
+///  Future<void> delete(String key) async {
+///    return _sharedPreferences.remove(key);
+///  }
+///
+///  @override
+///  Future<void> deleteAll() {
+///    return _sharedPreferences.clear();
+///  }
+///}
+///```
+///
+///## Hive:
+///```dart
+///class HiveImp implements IPersistStore {
+///  Box box;
+///
+///  @override
+///  Future<void> init() async {
+///    await Hive.initFlutter();
+///    box = await Hive.openBox('myBox');
+///  }
+///
+///  @override
+///  Object read(String key) {
+///      return box.get(key);
+///  }
+///
+///  @override
+///  Future<void> write<T>(String key, T value) async {
+///      return box.put(key, value);
+///  }
+///
+///  @override
+///  Future<void> delete(String key) async {
+///    return box.delete(key);
+///  }
+///
+///  @override
+///  Future<void> deleteAll() async {
+///    return box.clear();
+///  }
+///}
+///```
+///
 abstract class IPersistStore {
   ///Initialize the localStorage service
   Future<void> init();
@@ -97,6 +165,7 @@ class PersistStoreMock extends IPersistStore {
     return Future.value();
   }
 
+  ///Clear the store, Typically used indide setUp method of tests
   void clear() {
     store.clear();
     isAsyncRead = false;
