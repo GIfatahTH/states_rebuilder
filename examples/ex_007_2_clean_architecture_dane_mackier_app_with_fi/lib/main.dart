822373fed74e00ef2299b1ee05debb5e57d5b9a1
GIfatahTH <meltft@gmail.com>
2021-12-03 00:36:52 +0100

    update examples

--- examples/ex_007_2_clean_architecture_dane_mackier_app_with_fi/lib/main.dart
+++ examples/ex_007_2_clean_architecture_dane_mackier_app_with_fi/lib/main.dart
@@ -6,35 +6,50 @@ import 'ui/pages/comments_page/comments_page.dart';
 import 'ui/pages/login_page/login_page.dart';
 import 'ui/pages/posts_page/posts_page.dart';
 
+final navigator = RM.injectNavigator(
+  transitionsBuilder: RM.transitions.upToBottom(),
+  routes: {
+    '/': (data) => LoginPage(),
+    '/posts': (data) => PostsPage(),
+    '/comments': (data) => CommentsPage(post: data.arguments as Post),
+  },
+);
+
 void main() {
-  //Set transitionBuilder to be one of the four predefined ones.
-  //You can create yours
-  RM.navigate.transitionsBuilder = RM.transitions.upToBottom();
   runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    // Navigator 2
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(),
-      //As we will use states_rebuilder navigator
-      //we assign its key to the navigator key
-      navigatorKey: RM.navigate.navigatorKey,
-      //To use named route, and to make them use the defined
-      //transition builder, delegate to states_rebuilder the o
-      //onGenerateRoute.
-      //
-      //The RM.navigate.onGenerateRoute takes a map of your routes and
-      //returns the onGenerateRoute.
-      onGenerateRoute: RM.navigate.onGenerateRoute(
-        {
-          '/': (data) => LoginPage(),
-          '/posts': (data) => PostsPage(),
-          '/comments': (data) => CommentsPage(post: data.arguments as Post),
-        },
-      ),
+      routerDelegate: navigator.routerDelegate,
+      routeInformationParser: navigator.routeInformationParser,
     );
+
+    // For Navigator 1
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(),
+    //   //As we will use states_rebuilder navigator
+    //   //we assign its key to the navigator key
+    //   navigatorKey: RM.navigate.navigatorKey,
+    //   //To use named route, and to make them use the defined
+    //   //transition builder, delegate to states_rebuilder the o
+    //   //onGenerateRoute.
+    //   //
+    //   //The RM.navigate.onGenerateRoute takes a map of your routes and
+    //   //returns the onGenerateRoute.
+    //   onGenerateRoute: RM.navigate.onGenerateRoute(
+    //     {
+    //       '/': (data) => LoginPage(),
+    //       '/posts': (data) => PostsPage(),
+    //       '/comments': (data) => CommentsPage(post: data.arguments as Post),
+    //     },
+    //   ),
+    // );
   }
 }
