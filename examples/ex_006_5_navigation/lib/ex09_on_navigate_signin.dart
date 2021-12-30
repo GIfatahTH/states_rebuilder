92d824c4650d51b1a2f8a3b33796b3883ce00408
AmosLai <alexlai5128@gmail.com>
2021-12-30 22:48:37 +0800

    Update ex09_on_navigate_signin.dart

--- examples/ex_006_5_navigation/lib/ex09_on_navigate_signin.dart
+++ examples/ex_006_5_navigation/lib/ex09_on_navigate_signin.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 
-// Example of navigation guard that used the API InjectedNavigator.onNavigate to achive redirection when auth status is invaild.
+// Example of navigation guard that used the API `InjectedNavigator.onNavigate`
+// to achive redirection when auth status is invaild.
 
 void main() {
   runApp(const MyApp());
